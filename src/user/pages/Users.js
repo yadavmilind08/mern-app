10a37bdacd65c8d993d2ba845cc7039ca7c266f0
Milind Yadav <yadavmilind08@gmail.com>
2021-04-22 16:55:04 +0530

    Added User List Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Mk",
+      image: "https://images.com/image",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
